8af6af892bea6956d1e0ec9a3edefd3c0215c352
== <=>
2025-11-29 17:44:06 +0800

    新增外星人入侵

---
+++ game/bullet.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame.sprite import Sprite
+
+# 定义一个子弹类
+class Bullet(Sprite):
+    def __init__(self, ai_game):
+        super().__init__()
+        # 在飞船的当前位置创建子弹
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+        self.color = self.settings.bg_color
+
+        # 在（0，0） 出创建一个表示子弹的矩形，并且把位置放正确咯
+        self.rect = pygame.Rect(0, 0, self.settings.bg_width, self.settings.bg_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+        self.y = float(self.rect.y)
---
+++ game/setting.py
@@ -0,0 +1,12 @@
+class Settings:
+
+
+    def __init__(self):
+        self.screen_width = 1920
+        self.screen_height = 1080
+        self.bg_color = (0, 0, 0)
+        self.ship_speed  = 15
+        self.bullet_speed = 30.0
+        self.bullet_width = 4
+        self.bullet_height = 20
+        self.bullet_color = (60, 60, 60)
